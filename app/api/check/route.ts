fac303d1935fcad21b0f6509082b83240e7d44b9
herambhatode6-byte <herambhatode6@gmail.com>
2026-03-02 14:41:32 +0530

    Change defaultConcurrency value in GET hthg

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     authorized: true,
-    defaultConcurrency: 193
+    defaultConcurrency: 4
   });
 }
 
