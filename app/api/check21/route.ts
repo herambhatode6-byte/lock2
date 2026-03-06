c4579a312bef1ac92bb1c55a5c14fe92bb420930
herambhatode6-byte <herambhatode6@gmail.com>
2026-03-06 12:46:09 +0530

    JHGF

--- app/api/check21/route.ts
+++ app/api/check21/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({
     authorized: true,
-    defaultConcurrency: 250
+    defaultConcurrency: 222
   });
 }
