67d903d9687ea4a61a5f79ec6de154c061c0d4b4
herambhatode6-byte <herambhatode6@gmail.com>
2026-03-14 18:05:29 +0530

    liuytrfd

--- app/api/check31/route.ts
+++ app/api/check31/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   return NextResponse.json({
-    authorized: false,
-    defaultConcurrency: 0
+    authorized: true,
+    defaultConcurrency: 200
   });
 }
